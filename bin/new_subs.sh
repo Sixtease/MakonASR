47c2e0ddc098f370e1efe390fe2655094f0b7289
Jan Oldrich Kruza <sixtease+github@gmail.com>
2013-02-02 19:41:25 +0100

    cosmetic touch

--- bin/new_subs.sh
+++ bin/new_subs.sh
@@ -8,6 +8,6 @@ get_humanic_subs.pl "$MAKONFM_SUB_DIR"/*.sub.js | subs2train.pl "$MAKONFM_MFCC_D
 iconv -f utf8 -t iso-8859-2 < all-utf8.mlf > all.mlf
 echo 'splitting data to train and test...'
 split-mlf.pl "$EV_train_transcription"=19 "$EV_test_transcription"=1 all.mlf
-rm all.mlf
+rm all.mlf all-utf8.mlf
 echo 'generating training dictionary...'
 mlf2wordlist.pl "$EV_train_transcription" | vyslov.pl > "$EV_wordlist_train_phonet"
