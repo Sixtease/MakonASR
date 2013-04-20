1806b4abbcd23093d25dd28050f20b2bcfb14c76
Jan Oldrich Kruza <sixtease+github@gmail.com>
2013-04-20 15:04:30 +0200

    parallelisation debug

--- bin/batch-apply.sh
+++ bin/batch-apply.sh
@@ -7,11 +7,11 @@ if [ -z "$WORKDIR" ]; then echo "WORKDIR must be set"; exit 1; fi
 RECOUTDIR="${RECOUTDIR:-$WORKDIR}"
 
 ls "$@" | while read f; do
-    rm "$TEMPDIR"/*.* "$CHUNKDIR"/* 2> /dev/null
     stem=`basename "$f" | sed 's/\.sub\.js$//'`
     export TEMPDIR="$WORKDIR/$stem"
     export CHUNKDIR="$TEMPDIR/chunks"
-    if [ mkdir --verbose "$TEMPDIR" ]; then : ; else continue; fi
+    if [ mkdir "$TEMPDIR" 2>/dev/null ]; then echo "taking $stem" ; else echo "skipping $stem"; continue; fi
+    mkdir "$CHUNKDIR"
     split-by-subs.pl "$f" | split-mfcc.pl
 #    if [ -e "$RECOUTDIR/recout-$stem" ]; then : ; else
     recognize-splitted.pl "$stem" "$CHUNKDIR" > "$RECOUTDIR/recout-$stem"
