66e85deffaf125685dfce3eefe00a5c5bf427b4e
Jan Oldrich Kruza <sixtease+github@gmail.com>
2013-04-20 14:59:00 +0200

    Fixed parallelisation (hopefully)

--- bin/batch-apply.sh
+++ bin/batch-apply.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
-if [ -z "$MFCCDIR"  ]; then echo "MFCCDIR must be set" ; exit 1; fi
-if [ -z "$SUBDIR"   ]; then echo "SUBDIR must be set"  ; exit 1; fi
-if [ -z "$TEMPDIR"  ]; then echo "TEMPDIR must be set" ; exit 1; fi
-if [ -z "$CHUNKDIR" ]; then echo "CHUNKDIR must be set"; exit 1; fi
-RECOUTDIR="${RECOUTDIR:-$TEMPDIR}"
+if [ -z "$MFCCDIR" ]; then echo "MFCCDIR must be set"; exit 1; fi
+if [ -z "$SUBDIR"  ]; then echo "SUBDIR must be set" ; exit 1; fi
+if [ -z "$WORKDIR" ]; then echo "WORKDIR must be set"; exit 1; fi
+
+RECOUTDIR="${RECOUTDIR:-$WORKDIR}"
 
 ls "$@" | while read f; do
     rm "$TEMPDIR"/*.* "$CHUNKDIR"/* 2> /dev/null
     stem=`basename "$f" | sed 's/\.sub\.js$//'`
-    if [ -e "$SUBDIR/$stem.sub.js" ]; then
-        echo "Sub exists: $stem ($SUBDIR/$stem.sub.js)"
-        continue
-    else
-        touch "$SUBDIR/$stem.sub.js"
-    fi
+    export TEMPDIR="$WORKDIR/$stem"
+    export CHUNKDIR="$TEMPDIR/chunks"
+    if [ mkdir --verbose "$TEMPDIR" ]; then : ; else continue; fi
     split-by-subs.pl "$f" | split-mfcc.pl
 #    if [ -e "$RECOUTDIR/recout-$stem" ]; then : ; else
     recognize-splitted.pl "$stem" "$CHUNKDIR" > "$RECOUTDIR/recout-$stem"
