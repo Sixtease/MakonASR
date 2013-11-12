7e6cef58aa22909c419e2dfaa8870e76c49cf6f7
Jan Oldrich Kruza <sixtease+github@gmail.com>
2013-11-12 16:56:58 +0100

    recognize-splitted now uses LM given by env or args

--- bin/recognize-splitted.pl
+++ bin/recognize-splitted.pl
@@ -11,10 +11,14 @@ use HTKout2subs;
 
 my $subdir   = $ENV{SUBDIR};
 my $tempdir  = $ENV{TEMPDIR};
+my $lmf      = $ENV{EV_LMf};
+my $lmb      = $ENV{EV_LMb};
 
 GetOptions(
     'subdir=s'  => \$subdir,
     'tempdir=s' => \$tempdir,
+    'lmf=s'     => \$lmf,
+    'lmb=s'     => \$lmb,
 );
 
 die 'EV_homedir must be set' if not $ENV{EV_homedir};
@@ -24,7 +28,7 @@ my ($stem, $chunks_dir) = @ARGV;
 my @mfccs = glob("$chunks_dir/*.mfcc");
 generate_scp("$tempdir/mfcc.scp", \@mfccs);
 
-my $cmd = qq(julius -h hmms/hmmmodel -filelist "$tempdir/mfcc.scp" -nlr DATA/LM/tg.arpa -nrl DATA/LM/tgb.arpa -v DATA/wordlist/wl-test-phonet -hlist hmms/phones -walign -palign -input mfcfile -fallback1pass);
+my $cmd = qq(julius -h hmms/hmmmodel -filelist "$tempdir/mfcc.scp" -nlr "$lmf" -nrl "$lmb" -v DATA/wordlist/wl-test-phonet -hlist hmms/phones -walign -palign -input mfcfile -fallback1pass);
 print STDERR "$cmd\n";
 my $err = system($cmd);
 die "julius failed with status $err" if $err;
