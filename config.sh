dd118f7df2d36485251b59a42133d45c5103fb1d
Jan Oldrich Kruza <sixtease+github@gmail.com>
2016-10-26 11:56:46 +0200

    heldout ratio is specified as round number

--- config.sh
+++ config.sh
@@ -42,7 +42,7 @@ export EV_iter_triphones=3
 export EV_iter_mixtures=4
 
 export EV_thread_cnt=2
-export EV_heldout_ratio=39
+export EV_heldout_ratio=40
 
 export EV_corpus="$ASR_ROOT/DATA/corpus"
 export EV_word_blacklist="$ASR_ROOT/DATA/wordlist/blacklist"
