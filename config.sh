c938810dfb9ab426bd8db5fbc32354975615e25f
Jan Oldrich Kruza <sixtease+github@gmail.com>
2016-11-11 00:36:47 +0100

    source EV config from config.sh

--- config.sh
+++ config.sh
@@ -64,3 +64,5 @@ export MAKONFM_MP3_DIR='/home/sixtease/kroupy/Music/Makon/all/mp3'
 export MAKONFM_TEST_TRACKS="76-05B-Kaly-10:82-01A:kotouc-S01-c:86-05A-Brno-9.2.1986-3:90-45A"
 export MAKONFM_TEST_START_POS=60    # take testing data from 1 minute into the track
 export MAKONFM_TEST_END_POS=660     # up to minute 11 (10 minutes total per track)
+
+. "${EV_homedir}config.sh"
