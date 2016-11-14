7989fe615e6adda3b0aac0a828c259bd34abf4b5
Jan Oldrich Kruza <sixtease+github@gmail.com>
2016-11-14 20:53:22 +0100

    use modified knesser-ney discounting

--- bin/mklm.sh
+++ bin/mklm.sh
@@ -4,21 +4,29 @@ export BUILDING_LM=1
 
 DEST_LM="${DEST_LM:-$ASR_ROOT/DATA/LM}"
 DEST_WL="${DEST_WL:-$ASR_ROOT/DATA/wordlist}"
+mwl="$EV_workdir/merged-wordlist"
+mc="$EV_workdir/merged-corpus"
 
 get_humanic_subs.pl "$MAKONFM_SUB_DIR"/*.sub.js | sublm-mkdict-mkcorpus.pl "$EV_workdir/humsublm-wordlist" "$EV_workdir/humsublm-corpus"
 export LM_nonhumanic_only=1
 get_humanic_subs.pl "$MAKONFM_SUB_DIR"/*.sub.js | sublm-mkdict-mkcorpus.pl "$EV_workdir/sublm-wordlist" "$EV_workdir/sublm-corpus"
 unset LM_nonhumanic_only
 corpus2wordlist.pl < "$EV_corpus" > "$EV_workdir/txt-wordlist"
-merge-wordlists.pl 65000 "$EV_workdir/humsublm-wordlist" "$EV_workdir/sublm-wordlist" "$EV_workdir/txt-wordlist" > "$EV_workdir/merged-wordlist"
+merge-wordlists.pl 65000 "$EV_workdir/humsublm-wordlist" "$EV_workdir/sublm-wordlist" "$EV_workdir/txt-wordlist" > "$mwl"
+merge-wordlists.pl infinity "$EV_workdir/humsublm-wordlist" "$EV_workdir/sublm-wordlist" "$EV_workdir/txt-wordlist" > "$mwl-unlimited"
 
-cat "$EV_corpus" > "$EV_workdir/merged-corpus"
-cat "$EV_workdir/sublm-corpus" >> "$EV_workdir/merged-corpus"
-for s in {1..2}; do cat "$EV_workdir/humsublm-corpus" >> "$EV_workdir/merged-corpus"; done  # TODO: exclude test data, optimize weight
+cat "$EV_corpus" > "$mc"
+cat "$EV_workdir/sublm-corpus" >> "$mc"
+for s in {1..2}; do cat "$EV_workdir/humsublm-corpus" >> "$mc"; done  # TODO: exclude test data, optimize weight
 
-ngram-count -order 3 -vocab "$EV_workdir/merged-wordlist" -text "$EV_workdir/merged-corpus"  -lm "$DEST_LM/tg.arpa"
-reverse-corpus.pl < "$EV_workdir/merged-corpus" > "$EV_workdir/merged-corpusb"
-ngram-count -order 3 -vocab "$EV_workdir/merged-wordlist" -text "$EV_workdir/merged-corpusb" -lm "$DEST_LM/tgb.arpa"
-vyslov.pl < "$EV_workdir/merged-wordlist" > "$DEST_WL/wl-test-phonet"
+reverse-corpus.pl < "$mc" > "$mc"b
 
-#rm "$EV_workdir/sublm-wordlist" "$EV_workdir/sublm-corpus" "$EV_workdir/txt-wordlist" "$EV_workdir/merged-wordlist" "$EV_workdir/merged-corpus"
+ngram-count -order 3 -vocab "$mwl-unlimited" -text "$mc"  -kn1 "$EV_workdir/knf1" -kn2 "$EV_workdir/knf2"
+ngram-count -order 3 -vocab "$mwl-unlimited" -text "$mc"b -kn1 "$EV_workdir/knb1" -kn2 "$EV_workdir/knb2"
+
+ngram-count -order 3 -vocab "$mwl" -text "$mc"  -kndiscount1 -kn1 "$EV_workdir/knf1" -kndiscount2 -kn2 "$EV_workdir/knf2" -lm "$DEST_LM/tg.arpa"
+ngram-count -order 3 -vocab "$mwl" -text "$mc"b -kndiscount1 -kn1 "$EV_workdir/knb1" -kndiscount2 -kn2 "$EV_workdir/knb2" -lm "$DEST_LM/tgb.arpa"
+
+vyslov.pl < "$mwl" > "$DEST_WL/wl-test-phonet"
+
+echo rm "$EV_workdir/sublm-wordlist" "$EV_workdir/sublm-corpus" "$EV_workdir/txt-wordlist" "$mwl" "$mc" "$EV_workdir/knf1" "$EV_workdir/knf2"
