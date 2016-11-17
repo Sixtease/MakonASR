a87bc29d3ca7c8f9ecc0eaadc42197a3b04c5080
Jan Oldrich Kruza <sixtease+github@gmail.com>
2016-11-17 17:47:45 +0100

    lm: avoid test data

--- bin/mklm.sh
+++ bin/mklm.sh
@@ -7,10 +7,13 @@ DEST_WL="${DEST_WL:-$ASR_ROOT/DATA/wordlist}"
 mwl="$EV_workdir/merged-wordlist"
 mc="$EV_workdir/merged-corpus"
 
+# TODO: exclude test data
+export MAKONFM_SKIP_TEST_SUBS=1
 get_humanic_subs.pl "$MAKONFM_SUB_DIR"/*.sub.js | sublm-mkdict-mkcorpus.pl "$EV_workdir/humsublm-wordlist" "$EV_workdir/humsublm-corpus"
 export LM_nonhumanic_only=1
 get_humanic_subs.pl "$MAKONFM_SUB_DIR"/*.sub.js | sublm-mkdict-mkcorpus.pl "$EV_workdir/sublm-wordlist" "$EV_workdir/sublm-corpus"
 unset LM_nonhumanic_only
+unset MAKONFM_SKIP_TEST_SUBS
 corpus2wordlist.pl < "$EV_corpus" > "$EV_workdir/txt-wordlist"
 merge-wordlists.pl 65000 "$EV_workdir/humsublm-wordlist" "$EV_workdir/sublm-wordlist" "$EV_workdir/txt-wordlist" > "$mwl"
 merge-wordlists.pl infinity "$EV_workdir/humsublm-wordlist" "$EV_workdir/sublm-wordlist" "$EV_workdir/txt-wordlist" > "$mwl-unlimited"
