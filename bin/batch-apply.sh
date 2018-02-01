8c1a9ae1e808ad5367a9d09c2cad6d5d4f5572ef
Jan Oldrich Kruza <sixtease+github@gmail.com>
2018-02-01 10:39:11 +0100

    batch-apply enhancments

--- bin/batch-apply.sh
+++ bin/batch-apply.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-if [ -z "$MFCCDIR" ]; then echo "MFCCDIR must be set"; exit 1; fi
-if [ -z "$SUBDIR"  ]; then echo "SUBDIR must be set" ; exit 1; fi
+if [ -z "$MFCCDIR" ]; then export MFCCDIR="$MAKONFM_MFCC_DIR"; fi
+if [ -z "$MAKONFM_SUB_DIR"  ]; then echo "MAKONFM_SUB_DIR must be set" ; exit 1; fi
 if [ -z "$WORKDIR" ]; then echo "WORKDIR must be set"; exit 1; fi
 
 RECOUTDIR="${RECOUTDIR:-$WORKDIR/recout}"
 
-ls "$@" | while read f; do
+mkdir -p "$RECOUTDIR"
+
+for f in "$@"; do
     if [ -e '/tmp/SIGKILL' ]; then echo 'interrupt'; exit 1; fi
     stem=`basename "$f" | sed 's/\.sub\.js$//' | sed 's/\.mfcc$//'`
     export TEMPDIR="$WORKDIR/$stem"
     export CHUNKDIR="$TEMPDIR/chunks"
+    if [ -e "$f" ]; then :; else echo "no mfcc for $stem; skipping"; continue; fi
     if mkdir "$TEMPDIR" 2>/dev/null; then echo "taking $stem" ; else echo "skipping $stem"; continue; fi
     mkdir "$CHUNKDIR"
 
@@ -24,7 +27,7 @@ ls "$@" | while read f; do
     recognize-splitted.pl "$stem" "$CHUNKDIR" > "$RECOUTDIR/$stem"
 #    fi
     if [ -z "$RECOUT_ONLY" ]; then
-        julout2subs.pl "$CHUNKDIR/splits" "$stem" < "$RECOUTDIR/$stem" > "$SUBDIR/$stem.sub.js"
+        julout2subs.pl "$CHUNKDIR/splits" "$stem" < "$RECOUTDIR/$stem" > "$MAKONFM_SUB_DIR/$stem.sub.js"
     fi
     rm -R "$CHUNKDIR"/*.mfcc
 done
